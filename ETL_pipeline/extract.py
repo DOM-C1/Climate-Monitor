2700f57acfec230d3e794fa8c80a2fad01860542
dom-c1 <domgamin@gmail.com>
2024-04-24 13:27:03 +0100

    fixed small bug

--- ETL_pipeline/extract.py
+++ ETL_pipeline/extract.py
@@ -6,27 +6,27 @@ from dotenv import load_dotenv
 import requests
 
 
-def get_weather_details_for_week(latitude: int, longitude: int) -> dict:
-    """This function extract the weather details for the coming week."""
+def get_weather_details_for_week(latitude: float, longitude: float) -> dict:
+    """This function extracts the weather details for the coming week."""
     today = datetime.now()
     next_week = today + timedelta(weeks=1)
     today_str = today.isoformat(timespec='minutes')
     next_week_str = next_week.isoformat(timespec='minutes')
     response = requests.get(
         f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_hour={today_str}&end_hour={next_week_str}',
-        timeout=3)
+        timeout=6)
     return response.json()
 
 
-def get_weather_details_for_24hrs(latitude: int, longitude: int) -> dict:
+def get_weather_details_for_24hrs(latitude: float, longitude: float) -> dict:
     """This function extracts the weather details for the next 24hours."""
     today = datetime.now()
     tomorrow = today + timedelta(days=1)
     today_str = today.isoformat(timespec='minutes')
     tomorrow_str = tomorrow.isoformat(timespec='minutes')
     response = requests.get(
-        f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=cloud_cover,apparent_temperature,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_hour={today_str}&end_hour={tomorrow_str}',
-        timeout=3)
+        f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&minutely_15=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_minutely_15={today_str}&end_minutely_15={tomorrow_str}'
+        timeout=6)
     return response.json()
 
 
@@ -42,5 +42,5 @@ def get_air_quality(latitude: float, longitude: float) -> dict:
     load_dotenv()
     api_url = f'https://api.api-ninjas.com/v1/airquality?lat={latitude}&lon={longitude}'
     response = requests.get(
-        api_url, headers={'X-Api-Key': ENV['API_KEY']}, timeout=3)
+        api_url, headers={'X-Api-Key': ENV['API_KEY']}, timeout=6)
     return response.json()
