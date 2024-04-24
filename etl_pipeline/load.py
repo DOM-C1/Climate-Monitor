322aac65f8f4a228508fe4d9756058adc20b9491
arj0713 <arj07@live.co.uk>
2024-04-24 17:13:15 +0100

    add insert air quality

--- etl_pipeline/load.py
+++ etl_pipeline/load.py
@@ -1,11 +1,15 @@
+"""This file is the load aspect of the cloud monitor ETL Pipeline."""
+
 from os import environ as ENV
 from datetime import datetime
 
 from dotenv import load_dotenv
 from psycopg2 import connect, Connection
 
-def get_db_connection(config) -> Connection:
+
+def get_db_connection(config: dict) -> Connection:
     """Returns a connection to the database."""
+
     return connect(
         user=config["DB_USER"],
         password=config["DB_PASSWORD"],
@@ -13,6 +17,7 @@ def get_db_connection(config) -> Connection:
         port=config["DB_PORT"],
         database=config["DB_NAME"]
     )
+
 
 def insert_weather_report(conn: Connection, location_id: int) -> int:
     """Returns a weather report ID from the database having inserted a weather report."""
@@ -24,13 +29,13 @@ def insert_weather_report(conn: Connection, location_id: int) -> int:
             (%s, %s)
         RETURNING weather_report_id;
         """
-    
+
     with conn.cursor() as cur:
         cur.execute(q, (datetime.now(), location_id))
-        id = cur.fetchone()
+        weather_report_id = cur.fetchone()
     conn.commit()
 
-    return id
+    return weather_report_id
 
 
 def insert_forecast(conn: Connection, forecast: dict, weather_report_id: int) -> int:
@@ -45,13 +50,13 @@ def insert_forecast(conn: Connection, forecast: dict, weather_report_id: int) ->
             {vals}
         RETURNING forecast_id;
         """.format(cols=forecast.keys(), vals=forecast.values())
-    
+
     with conn.cursor() as cur:
         cur.execute(q)
-        id = cur.fetchone()
+        forecast_id = cur.fetchone()
     conn.commit()
 
-    return id
+    return forecast_id
 
 
 def insert_weather_alert(conn: Connection, weather_alert: dict, forecast_id: int) -> None:
@@ -63,7 +68,7 @@ def insert_weather_alert(conn: Connection, weather_alert: dict, forecast_id: int
         VALUES
             (%s, %s);
         """
-    
+
     with conn.cursor() as cur:
         cur.execute(q, (weather_alert["alert_type_id"],
                         forecast_id,
@@ -80,11 +85,26 @@ def insert_flood_warning(conn: Connection, flood_warning: dict, location_id: int
         VALUES
             (%s, %s, %s);
         """
-    
+
     with conn.cursor() as cur:
         cur.execute(q, (flood_warning["severity_level_id"],
                         flood_warning["time_raised"],
                         location_id))
     conn.commit()
 
-    
+
+def insert_air_quality(conn: Connection, air_quality: dict, weather_report_id: int) -> None:
+    """Inserts an air quality reading to the database."""
+
+    q = """
+        INSERT INTO air_quality
+            (o3_concentration, severity_level_id, weather_report_id)
+        VALUES
+            (%s, %s, %s);
+        """
+
+    with conn.cursor() as cur:
+        cur.execute(q , (air_quality["o3_concentration"],
+                         air_quality["severity_level_id"],
+                         weather_report_id))
+    conn.commit()
