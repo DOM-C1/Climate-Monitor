22d22ede8f4a1f04c2ded4530fe98a3d14d3ff78
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-25 16:26:06 +0100

    stop times overlapping

--- ETL_pipeline/extract.py
+++ ETL_pipeline/extract.py
@@ -6,20 +6,31 @@ from dotenv import load_dotenv
 import requests
 
 
-def time_rounder(t):
-    return (t.replace(second=0, microsecond=0, minute=(t.minute // 15 * 15)))
+def time_rounder(timestamp: datetime, get_fifteen: bool = True) -> datetime:
+    """Obtains the most recent 15 min time, or the most recent hour"""
+    if get_fifteen:
+        return (timestamp.replace(second=0, microsecond=0, minute=(timestamp.minute // 15 * 15)))
+    return (timestamp.replace(second=0, microsecond=0, minute=0))
 
 
-today = time_rounder(datetime.now())
-tomorrow = today + timedelta(days=1)
-next_week = today + timedelta(weeks=1, hours=1)
-tomorrow_str = tomorrow.isoformat(timespec='minutes')
-next_week_str = next_week.isoformat(timespec='minutes')
-today_str = today.isoformat(timespec='minutes')
+def generate_time_strings(get_today: bool = True) -> list[str]:
+    """Returns the times for either today and tomorrow, or tomorrow and next week."""
+    if get_today:
+        today = time_rounder(datetime.now())
+        time_str = today.isoformat(timespec='minutes')
+        tomorrow = time_rounder(today, get_fifteen=False) + timedelta(days=1)
+    else:
+        today = time_rounder(datetime.now(), get_fifteen=False)
+        next_week = today + timedelta(weeks=1)
+        time_str = next_week.isoformat(timespec='minutes')
+        tomorrow = today + timedelta(hours=25)
+    tomorrow_str = tomorrow.isoformat(timespec='minutes')
+    return time_str, tomorrow_str
 
 
 def get_weather_details_for_week(latitude: float, longitude: float) -> dict:
     """This function extracts the weather details for the coming week."""
+    next_week_str, tomorrow_str = generate_time_strings(get_today=False)
     response = requests.get(
         f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_hour={tomorrow_str}&end_hour={next_week_str}&timezone=Europe/London',
         timeout=6)
@@ -28,6 +39,7 @@ def get_weather_details_for_week(latitude: float, longitude: float) -> dict:
 
 def get_weather_details_for_24hrs(latitude: float, longitude: float) -> dict:
     """This function extracts the weather details for the next 24hours."""
+    today_str, tomorrow_str = generate_time_strings()
     response = requests.get(
         f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&minutely_15=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_minutely_15={today_str}&end_minutely_15={tomorrow_str}&timezone=Europe/London',
         timeout=6)
@@ -41,10 +53,10 @@ def get_flood_warning_json() -> dict:
     return response.json()
 
 
-def get_air_quality(latitude: float, longitude: float) -> dict:
+def get_air_quality(latitude: float, longitude: float, config: dict) -> dict:
     """This function gets the air quality given a latitude and longitude."""
     load_dotenv()
     api_url = f'https://api.api-ninjas.com/v1/airquality?lat={latitude}&lon={longitude}'
     response = requests.get(
-        api_url, headers={'X-Api-Key': ENV['API_KEY']}, timeout=6)
+        api_url, headers={'X-Api-Key': config['API_KEY']}, timeout=6)
     return response.json()
