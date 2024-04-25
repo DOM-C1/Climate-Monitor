ceea192d18ee5ed9588d48b647b352e8b47cd358
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-25 02:12:59 +0100

    reverse changes

--- database/initialise-database.sh
+++ database/initialise-database.sh
@@ -1,4 +1,4 @@
 echo "Connecting to DB"
 export $(cat .env | xargs)
 export PGPASSWORD=$DB_PASSWORD
-psql --host=$DB_HOST --port=$DB_PORT --username=$DB_USER --dbname=postgres
+psql --host=$DB_HOST --port=$DB_PORT --username=$DB_USER --dbname=postgres -f schema.sql
