009b5f7ffd232b6f76b23c0ffbb8bb839ccefb5d
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-25 15:36:21 +0100

    tmr str

--- ETL_pipeline/extract.py
+++ ETL_pipeline/extract.py
@@ -9,11 +9,12 @@ import requests
 def get_weather_details_for_week(latitude: float, longitude: float) -> dict:
     """This function extracts the weather details for the coming week."""
     today = datetime.now()
+    tomorrow = today + timedelta(days=1)
     next_week = today + timedelta(weeks=1)
-    today_str = today.isoformat(timespec='minutes')
+    tomorrow_str = tomorrow.isoformat(timespec='minutes')
     next_week_str = next_week.isoformat(timespec='minutes')
     response = requests.get(
-        f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_hour={today_str}&end_hour={next_week_str}',
+        f'https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&hourly=apparent_temperature,cloud_cover,relative_humidity_2m,lightning_potential,precipitation,precipitation_probability,rain,snowfall,temperature_2m,uv_index,visibility,wind_direction_10m,wind_gusts_10m,wind_speed_10m,weather_code&start_hour={tomorrow_str}&end_hour={next_week_str}',
         timeout=6)
     return response.json()
 
