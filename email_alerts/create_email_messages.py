3d911b798ebcd10334b22459bc13d79a1ab874d5
NMcKittie <nathankit66@gmail.com>
2024-04-26 11:57:48 +0100

    edits to the html

--- email_alerts/create_email_messages.py
+++ email_alerts/create_email_messages.py
@@ -7,7 +7,7 @@ def get_alert_msg(alert, type):
     """Creates an alert message depending on severity and type of alert."""
 
     if alert == 'Alert':
-        return f'{alert}! Higher {type}'
+        return f'{alert}! Elevated {type}'
     if alert == 'Warning':
         return f'{alert}! High {type}'
     if alert == 'Severe Warning':
@@ -21,7 +21,7 @@ def get_alert_visual(alert):
     if alert == 'Alert':
         return '#f3d300'
     if alert == 'Warning':
-        return 'orange'
+        return '#FF8300'
     if alert == 'Severe Warning':
         return 'red'
     return ''
@@ -33,7 +33,6 @@ def create_html_table_weather(user_alerts: list[list[str]]) -> str:
     table_style = "'border: 1px solid  black;text-align: left;padding: 8px;'"""
 
     table = f"""<table style=
-    'font-family: arial, sans-serif;
     border-collapse: collapse;
     padding: 2rem 0.5rem;
     background-color: white;'>
@@ -45,9 +44,9 @@ def create_html_table_weather(user_alerts: list[list[str]]) -> str:
         alert_msg = get_alert_msg(alert[2], alert[5])
         table += f"""<tr>
         <td style='border: 1px solid  black;text-align: left;padding: 8px; background-color: {get_alert_visual(alert[2])}; font-size: 30px;'>!</td>
-        <td style={table_style}>{alert[6].strftime("%H:%M:%S")}</td>
-        <td style={table_style}>{alert[3]}</td>
         <td style={table_style}>{alert_msg} {alert[-1]}</td>
+        <td style={table_style}>{alert[3]}</td>
+        <td style={table_style}>{alert[6].strftime("%H:%M:%S")}</td>
         </tr>"""
     table += "</table>"
     return table
@@ -91,7 +90,7 @@ def create_full_html_per_user(users_weather: list[list]) -> str:
     <head>
     <meta charset="UTF-16">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head><body style='justify-content: center;align-items: center;'>
+    </head><body style=''font-family: arial, sans-serif; background-color: white; justify-content: center;align-items: center;'>
     <h1>!!!!!Weather Alerts!!!!!</h1>"""
     weather_alerts = create_html_table_weather(users_weather)
     air_quality = create_html_air_quality(users_weather)
@@ -114,7 +113,7 @@ def assign_messages_to_recipients(recipients: dict) -> dict:
 
 if __name__ == '__main__':
 
-    test_data = {'trainee.nathan.mckittrick@sigmalabs.co.uk': [['weather_alert', 1, 'Alert', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 15, 42), 'Clear Sky', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 3.3, '&#x1F506'], ['weather_alert', 2, 'Alert', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 15, 57), 'Clear Sky', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 3.0, '&#x1F506'], [
+    test_data = {'trainee.nathan.mckittrick@sigmalabs.co.uk': [['weather_alert', 1, 'Severe Warning', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 15, 42), 'Clear Sky', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 3.3, '&#x1F506'], ['weather_alert', 2, 'Alert', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 15, 57), 'Clear Sky', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 3.0, '&#x1F506'], [
         'weather_alert', 3, 'Alert', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 16, 12), 'Mainly Clear', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 2.75, '&#x1F506'], ['weather_alert', 4, 'Alert', 'Liverpool', 'Liverpool City Region', 'UV-index', datetime.datetime(2024, 4, 25, 16, 27), 'Partly Cloudy', datetime.datetime(2024, 4, 25, 15, 42, 42, 19560), 2.55, '&#x1F506'], ['air_quality', 1, 'Alert', 'Liverpool', 'Liverpool City Region', 102], ['flood_warnings', 1, 'Warning', 'Liverpool', 'Liverpool City Region', datetime.datetime(2024, 4, 25, 15, 42)]]
     }
     x = assign_messages_to_recipients(test_data)
