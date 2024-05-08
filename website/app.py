5636d7a5559d2efdfa3ef9b88283b3a4e3fb457a
dom-c1 <domgamin@gmail.com>
2024-05-08 09:38:06 +0100

    fix bug where only locations that had forecasts would show up

--- website/app.py
+++ website/app.py
@@ -2,68 +2,117 @@
 from os import environ as ENV
 
 from dotenv import load_dotenv
-from flask import Flask, request, render_template
-
+from flask import Flask, request, jsonify
 from utils import get_details_from_post_code
-from utils_db import get_db_connection, get_id, setup_user_location, get_value_from_db
-
+from utils_db import get_db_connection, get_id, setup_user_location, get_value_from_db, \
+    check_row_exists, get_locations_for_user, update_loc_assignment, delete_user
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def homepage():
-    """This displays the homepage."""
-    return render_template('newsletter.html')
-
-
-@app.route('/submit-location', methods=['POST'])
-def submit_user():
-    """When a request is sent, the users new location is sent to the database."""
-    email = request.form['email']
-    postcode = request.form['postcode']
-    details = get_details_from_post_code(postcode)
+@app.route('/login', methods=['POST'])
+def login_user():
+    """This function validates the login of a user."""
+    load_dotenv()
     conn = get_db_connection(ENV)
-    print(type(conn))
-    user_id = get_id('user_details', 'email', email, conn)
-    if user_id == -1:
-        return render_template('user_not_found.html')
-
-    if details['status'] != 200:
-        return render_template('page_not_found.html')
-    alert_on = get_value_from_db(
-        'user_location_assignment', 'alert_opt_in', user_id, 'user_id', conn)
-    report_on = get_value_from_db(
-        'user_location_assignment', 'report_opt_in', user_id, 'user_id', conn)
-    name = get_value_from_db(
-        'user_details', 'name', user_id, 'user_id', conn)
-    setup_user_location(details, name, email, report_on, alert_on, conn)
-    return 'Location added!'
-
-
-@app.route('/location')
-def location_page():
-    """Displays the HTML on the website for the location route."""
-    return render_template('location_form.html')
+    data = request.json
+    email = data['email'].lower()
+    password = data['password']
+    if not check_row_exists(conn, 'user_details', 'email', email, 'password', password):
+        response = jsonify({'error': 'Invalid credentials'})
+        response.status_code = 400
+        return response
+    _id = get_id('user_details', 'email', email, conn)
+    name = get_value_from_db('user_details', 'name', _id, 'user_id', conn)
+    conn.close()
+    response = jsonify({'message': 'Login Successful', 'name': name})
+    response.status_code = 200
+    return response
 
 
 @app.route('/submit-user', methods=['POST'])
 def submit_location():
     """When a user signs-up, it gets sent through here and uploaded
        to the database."""
-    location_value = request.form['location']
-    name = request.form['name']
-    email = request.form['email']
-    sub_newsletter = request.form.get('newsletter', 'off') == 'on'
-    sub_alerts = request.form.get('alerts', 'off') == 'on'
+    data = request.json
+    location_value = data['location']
+    name = data['name']
+    email = data['email'].lower()
+    sub_newsletter = data['newsletter']
+    sub_alerts = data['alerts']
+    password = data['password']
     details = get_details_from_post_code(location_value)
     conn = get_db_connection(ENV)
-    if details['status'] == 200:
-        setup_user_location(details, name, email,
-                            sub_newsletter, sub_alerts, conn)
-        return 'User Added!'
+    setup_user_location(details, name, email,
+                        sub_newsletter, sub_alerts, password, conn)
+    response = jsonify({'message': 'User location added successfully'})
+    response.status_code = 200
+    return response
 
-    return render_template('cant_be_found_page.html')
+
+@app.route('/get_details', methods=['POST'])
+def get_details():
+    """This returns a dataframe for a particular users details."""
+    load_dotenv()
+    email = request.json['email']
+    password = request.json['password']
+
+    try:
+        conn = get_db_connection(ENV)
+        df = get_locations_for_user(conn, email)
+        if check_row_exists(conn, 'user_details', 'email', email, 'password', password):
+
+            if df.empty:
+                return jsonify({'message': 'No data found for the provided email'}), 404
+
+            df_json = df.to_json(orient='records')
+            return jsonify({'message': 'success', 'df': df_json}), 200
+        else:
+            return jsonify({'message': 'No data found for those credentials'}), 404
+
+    except Exception as e:
+        return jsonify({'error': f"An error occurred: {str(e)}"}), 500
+
+
+@app.route('/update_notifs', methods=['POST'])
+def update_notifs():
+    """Updates the notifications for a user."""
+    load_dotenv()
+    email = request.json['email']
+    password = request.json['password']
+    alerts = request.json['alerts']
+    reports = request.json['reports']
+
+    conn = get_db_connection(ENV)
+    if check_row_exists(conn, 'user_details', 'email', email, 'password', password):
+        for dict in alerts:
+            _id = dict['id']
+            value = dict['value']
+            update_loc_assignment(
+                conn, 'user_location_id ', _id, 'alert_opt_in', value)
+        for dict in reports:
+            _id = dict['id']
+            value = dict['value']
+            update_loc_assignment(
+                conn, 'user_location_id ', _id, 'report_opt_in', value)
+
+        return jsonify({'message': 'success'}), 200
+
+    return jsonify({'message': 'No data found for those credentials'}), 404
+
+
+@ app.route('/delete_user', methods=['POST'])
+def del_user():
+    """If a user wants to delete their account, they can do so through here."""
+    email = request.json['email']
+    password = request.json['password']
+    load_dotenv()
+    conn = get_db_connection(ENV)
+    if check_row_exists(conn, 'user_details', 'email', email, 'password', password):
+        _id = get_id('user_details', 'email', email, conn)
+        delete_user(conn, _id)
+        return jsonify({'message': 'success'}), 200
+    return jsonify({'message': 'Error deleting user'}), 500
 
 
 if __name__ == '__main__':
--- website/utils_db.py
+++ website/utils_db.py
@@ -3,6 +3,7 @@ from flask import render_template
 from psycopg2 import connect
 from psycopg2.extensions import connection
 from utils import get_standard_long_lat, get_postcode_long_lat, get_location_names
+import pandas as pd
 
 ERROR_CODE = -1
 
@@ -26,12 +27,12 @@ def add_to_database(table: str, data: dict, conn: connection) -> None:
     values = list(data.values())
     with conn.cursor() as cur:
         cur.execute(query, values)
-        conn.commit()
+    conn.commit()
 
 
 def get_id(table: str, column: str, value: str, conn: connection) -> int:
-    """Given the table name, column and a value, checks whether that 
-    value exists and return it's ID if it does exist; a return value of -1 
+    """Given the table name, column and a value, checks whether that
+    value exists and return it's ID if it does exist; a return value of -1
     indicates that value doesn't exist."""
     query = f"SELECT * FROM {table} WHERE {column} = %s"
     with conn.cursor() as cur:
@@ -53,16 +54,13 @@ def get_loc_id(location_name: str, conn: connection) -> int:
     return result[0] if result else ERROR_CODE
 
 
-def setup_user_location(details: dict, name: str, email: str,
-                        sub_newsletter: bool, sub_alerts: bool,
-                        conn: connection) -> str:
+def setup_user_location(details, name, email, sub_newsletter, sub_alerts, conn) -> str:
     """This sets up location tracking for a user, if the user exists then it just adds a new
        location, otherwise, it sets up the new user too."""
     longitude, latitude = get_postcode_long_lat(details)
     location_name, county, country = get_location_names(longitude, latitude)
     longitude, latitude = get_standard_long_lat(location_name)
     country_id = get_id('country', 'name', country, conn)
-
     if country_id == ERROR_CODE:
         return render_template('page_not_found.html')
 
@@ -72,16 +70,13 @@ def setup_user_location(details: dict, name: str, email: str,
         county_data = {'name': county, 'country_id': country_id}
         add_to_database('county', county_data, conn)
         county_id = get_id('county', 'name', county, conn)
-
     user_data = {'email': email, 'name': name}
     user_id = get_id('user_details', 'email', email, conn)
 
     if user_id == ERROR_CODE:
         add_to_database('user_details', user_data, conn)
         user_id = get_id('user_details', 'email', email, conn)
-
-    loc_id = get_loc_id(location_name, conn)
-
+    loc_id = get_loc_id(longitude, latitude, conn)
     if loc_id == ERROR_CODE:
         location_data = {'loc_name': location_name,
                          'county_id': county_id, 'longitude': longitude, 'latitude': latitude}
@@ -92,15 +87,79 @@ def setup_user_location(details: dict, name: str, email: str,
                      'report_opt_in': sub_newsletter, 'alert_opt_in': sub_alerts}
     add_to_database('user_location_assignment',
                     user_loc_data, conn)
+
     conn.close()
     return ''
 
 
 def get_value_from_db(table: str, column: str, _id: str, id_name: str, conn) -> str:
-    """Extract the value associated with a particular ID, 
+    """Extract the value associated with a particular ID,
        table and column."""
     query = f"SELECT {column} FROM {table} WHERE {id_name} = %s"
     with conn.cursor() as cur:
         cur.execute(query, (_id,))
         result = cur.fetchone()
     return result[0] if result else ''
+
+
+def check_row_exists(conn, table_name, column1, value1, column2, value2) -> bool:
+    """
+    Check if a row exists in the database that satisfies the conditions for two columns."""
+    query = f"""
+    SELECT EXISTS (
+        SELECT 1 FROM {table_name}
+        WHERE {column1} = %s AND {column2} = %s
+    );
+    """
+
+    with conn.cursor() as cur:
+
+        cur.execute(query, (value1, value2))
+        exists = cur.fetchone()[0]
+    return True if exists else False
+
+
+def query_to_df(conn: connection, query: str) -> pd.DataFrame:
+    """Executes a SQL query and returns the results as a pandas DataFrame."""
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
+        columns = [desc[0] for desc in cur.description]
+
+    return pd.DataFrame(rows, columns=columns)
+
+
+def get_locations_for_user(conn: connection, email: str) -> pd.DataFrame:
+    user_details_query = """
+        SELECT *
+        FROM user_details AS UD
+        JOIN user_location_assignment ON UD.user_id = user_location_assignment.user_id
+        JOIN location ON user_location_assignment.loc_id = location.loc_id
+        JOIN weather_report ON location.loc_id = weather_report.loc_id
+    """
+    user_details = query_to_df(conn, user_details_query)
+    return user_details[['email'] == email]
+
+
+def update_loc_assignment(conn: connection, _id_name: str, _id: int, column: str, value: str) -> None:
+    """ Updates user location assignment so that if a user updates their details 
+        it gets updated through this."""
+    query = f"""
+    UPDATE user_location_assignment
+    SET {column} = %s
+    WHERE {_id_name} = %s;
+    """
+    values = (value, _id)
+    with conn.cursor() as cur:
+        cur.execute(query, values)
+    conn.commit()
+    return None
+
+
+def delete_user(conn, _id):
+    """Deletes a users records."""
+    with conn.cursor() as cur:
+        cur.execute(
+            "DELETE FROM user_location_assignment WHERE user_id = %s;", (_id,))
+        cur.execute("DELETE FROM user_details WHERE user_id = %s;", (_id,))
+        conn.commit()
